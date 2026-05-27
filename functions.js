73cb9da9a32f49f793bd35e54bec28c5ea147443
hendrickscial <hendrickscial@gmail.com>
2026-05-27 14:58:19 +0200

    :car:

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return "Hello, " +name+ "!";
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+let greetingMessage = greet("Cial");
+let sumResult = 10 + 15
+
+console.log(greetingMessage);
+console.log("Sum: "+sumResult+"");
